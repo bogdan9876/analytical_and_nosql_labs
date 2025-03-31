d5c2d1de12a2a78b4d407954fc57403ecb9c5f4c
bogdan9876 <bogdankryveiko@gmail.com>
2025-03-31 18:21:21 +0300

    location done

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 import fs from 'fs';
 import faker from 'faker';
+import { Country, State, City }  from 'country-state-city';
 import { Parser } from 'json2csv';
 const { commerce, datatype } = faker;
+
+const getRandomLocation = () => {
+  const countries = Country.getAllCountries();
+  const randomCountry = faker.random.arrayElement(countries);
+  
+  const states = State.getStatesOfCountry(randomCountry.isoCode);
+  if (states.length === 0) return null;
+
+  const randomState = faker.random.arrayElement(states);
+  
+  const cities = City.getCitiesOfState(randomCountry.isoCode, randomState.isoCode);
+  if (cities.length === 0) return null;
+
+  return {
+    location_city: cities.length > 0 ? faker.random.arrayElement(cities).name : null,
+    location_region: randomState.name,
+    location_country: randomCountry.name
+  };
+};
 
 const generateFakeData = (numRecords) => {
   const fakeData = [];
 
   for (let i = 0; i < numRecords; i++) {
+    const location = getRandomLocation();
+
+    if (!location) continue;
+
     const sale = {
       id: i + 1,
       site_price: parseFloat(commerce.price(500, 10000, 2)),
@@ -20,9 +44,9 @@ const generateFakeData = (numRecords) => {
       user_birth_year: datatype.number({ min: 1960, max: 2005 }),
       user_career: faker.name.jobTitle(),
       user_sex: datatype.boolean() ? 'Male' : 'Female',
-      location_city: faker.address.city(),
-      location_region: faker.address.state(),
-      location_country: faker.address.country(),
+      location_city: location.location_city,
+      location_region: location.location_region,
+      location_country: location.location_country,
       action_date: faker.date.past(2).toISOString().split('T')[0],
       provider_name: faker.company.companyName(),
       site_type: faker.random.arrayElement(['Blog', 'E-commerce', 'Portfolio', 'Business', 'Personal'])
